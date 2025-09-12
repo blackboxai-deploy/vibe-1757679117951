d9175da0d11998f845922580349ae048346aba8f
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 17:42:08 +0530

    Add src/app/summary/page.tsx

---
+++ src/app/summary/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { SessionSummary } from '@/components/session/SessionSummary';
+
+export default function SummaryPage() {
+  return (
+    <div>
+      <SessionSummary />
+    </div>
+  );
+}
