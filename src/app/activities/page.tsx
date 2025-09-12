332a44c3114579f1b59302fcb6016e0d4ac942dc
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 17:42:06 +0530

    Add src/app/activities/page.tsx

---
+++ src/app/activities/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ActivityGrid } from '@/components/activities/ActivityGrid';
+
+export default function ActivitiesPage() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <ActivityGrid />
+    </div>
+  );
+}
